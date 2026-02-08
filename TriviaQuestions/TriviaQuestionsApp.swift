a83abb46729adf66d27be10c36a06ce50d31e63e
Nathan Tugwell <nathan.tugwell@me.com>
2026-02-08 18:46:33 +0000

    Initial Commit

---
+++ TriviaQuestions/TriviaQuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriviaQuestionsApp.swift
+//  TriviaQuestions
+//
+//  Created by Nathan Tugwell on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct TriviaQuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
